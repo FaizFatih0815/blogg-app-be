4cf8c8eadf848e91574931560e0b20e4aca5ef3c
FaizFatih0815 <139619851+FaizFatih0815@users.noreply.github.com>
2025-07-10 20:21:25 +0700

    feat:add register (#1)

--- src/modules/auth/auth.service.ts
+++ src/modules/auth/auth.service.ts
@@ -4,16 +4,20 @@ import { ApiError } from "../../utils/api-error";
 import { PasswordService } from "./password.service";
 import { TokenService } from "./token.service";
 import { JWT_SECRET } from "../../config/env";
+import { RegisterDTO } from "./dto/register.dto";
+import { MailService } from "../mail/mail.service";
 
 export class AuthService {
   private prisma: PrismaService;
   private passwordService: PasswordService;
   private tokenService: TokenService;
+  private mailService: MailService;
 
   constructor() {
     this.prisma = new PrismaService();
     this.passwordService = new PasswordService();
     this.tokenService = new TokenService();
+    this.mailService = new MailService();
   }
 
   login = async (body: Pick<User, "email" | "password">) => {
@@ -44,5 +48,33 @@ export class AuthService {
     const { password, ...userWithoutPassword } = user;
 
     return { ...userWithoutPassword, accessToken };
+  };
+
+  register = async (body: RegisterDTO) => {
+    const existingEmail = await this.prisma.user.findFirst({
+      where: { email: body.email },
+    });
+
+    if (existingEmail) {
+      throw new ApiError("Email already exist", 400);
+    }
+
+    const hashedPassword = await this.passwordService.hashPassword(
+      body.password
+    );
+
+    const newUser = await this.prisma.user.create({
+      data: { ...body, password: hashedPassword },
+      omit: { password: true },
+    });
+
+    await this.mailService.sendEmail(
+      body.email,
+      "Welcome to BlogHub",
+      "welcoming",
+      { name: body.name }
+    );
+
+    return newUser;
   };
 }
